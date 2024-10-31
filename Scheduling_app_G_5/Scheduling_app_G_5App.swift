f704119af68d36dd7f6c18d4862908f43f1c5290
Digaant Dogra <dogra057@gmail.com>
2024-10-31 12:06:20 -0400

    Initial Commit

---
+++ Scheduling_app_G_5/Scheduling_app_G_5App.swift
@@ -0,0 +1,17 @@
+//
+//  Scheduling_app_G_5App.swift
+//  Scheduling_app_G_5
+//
+//  Created by Digaant Dogra on 2024-10-31.
+//
+
+import SwiftUI
+
+@main
+struct Scheduling_app_G_5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
