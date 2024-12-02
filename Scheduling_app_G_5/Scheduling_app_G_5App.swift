54ae9d5609dc979d97efaccfa6731645f7441e8a
Digaant Dogra <dogra057@gmail.com>
2024-12-01 23:33:59 -0500

    User Authentication Firebase Initialized

--- Scheduling_app_G_5/Scheduling_app_G_5App.swift
+++ Scheduling_app_G_5/Scheduling_app_G_5App.swift
@@ -6,9 +6,14 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct Scheduling_app_G_5App: App {
+    init(){
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
---
+++ Scheduling_app_G_5/User.swift
@@ -0,0 +1,20 @@
+//
+//  User.swift
+//  Scheduling_app_G_5
+//
+//  Created by Digaant Dogra on 2024-11-19.
+//
+
+import Foundation
+
+struct User{
+    let userName:String
+    let password:String
+    
+    func isLogedIn(){
+        
+    }
+    func isVerified(){
+        
+    }
+}
